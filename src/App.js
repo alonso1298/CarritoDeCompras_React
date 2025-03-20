380aec9c545bf87518e0ba0dc8403ff7b8540035
alonso1298 <alonsosagrerogranados@gmail.com>
2025-03-19 20:27:01 -0600

    Se agrega el componente de navegacion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Productos from './components/Productos';
 import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import Title from './components/Title';
 
 class App extends Component {
